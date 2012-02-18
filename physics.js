c7f34760bb7e28f3bede48a2cdc20535f11d15c3
Sharad Vikram <sharad.vikram@gmail.com>
2012-02-17 23:52:44 -0600

    first version of hopefully working physics

--- physics.js
+++ physics.js
@@ -1,29 +1,54 @@
 var Bridge = require('./bridge').Bridge;
 var bridge = new Bridge({host:'136.152.39.187'});
-var ball = {x:0,y:0,radius:5};
+var ball = {x:0,y:0,radius:20};
 var paddle;
 var vel = {x:0,y:1};
 var running = true;
 var hard, soft;
 physics = {
-	start:function(h, s){
-		hard = h;
+	start:function(b, s, x, y){
+		console.log("sup");
+		boxes = b;
 		soft = s;
+		ball.x = x;
+		ball.y = y;	
 	},
 	update:function(){
-		next = 0;
-		for (var p=0;p<hard.length;p++){
-			if (next == hard.length-1) {
-				next = 0;
-			} else {
-				next = p+1;
-			}
-			edge = [{x:hard[p].x,y:hard[p].y},{x:hard[next].x,y:hard[next].y}];	
-			if ((ball.x<=edge[0].x && ball.x>=edge[1]) || (ball.x<=edge[1].x && ball.x>=edge[1])){
-				console.log("left of edge");	
+		curBox =  -1;
+		nextXBox = -1;
+		nextYBox = -1;
+		for (var box in boxes){
+			if (util.contains(ball,boxes[box])){
+				curBox = box;
 			}
 		}
+		nextXBall = {x:(ball.x+=vel.x),y:(ball.y),radius:ball.radius};
+		nextYBall = {x:(ball.x),y:(ball.y+=vel.y),radius:ball.radius};
+		for (var box in boxes){
+			if (util.contains(nextXBall,boxes[box])){
+				nextXBox = box;
+			}
+			if (util.contains(nextYBall,boxes[box])){
+				nextYBox = box;
+			}
+		}	
+		if (box != nextXBox){
+			vel.x = -vel.x;
+		}
+		if (box != nextYBox){
+			vel.y = -vel.y;
+		}	
+		ball.x += vel.x;
+		ball.y += vel.y;	
+		return {"ball":ball};
 	}
 }
-physics.start([{x:0,y:0},{x:1,y:0},{x:0.5,y:0.5}],[]);
-physics.update();
+util = {
+	contains:function(ball,box){
+		console.log('contains');
+		return ((ball.x+ball.radius)>=box[0].x && (ball.x-ball.radius)<=box[1].x) && ((ball.y+ball.radius)>=box[1].y && (ball.y-ball.radius)<=box[0].y)
+	}
+}
+bridge.ready(function(){
+	bridge.publishService('physics',physics);
+});
