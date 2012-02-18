53908aa871f235945f7bcff26cbdb7859524ced9
Gerald <geraldgfong@gmail.com>
2012-02-18 01:10:55 -0800

    minor css edits

--- admin/public/js/display.js
+++ admin/public/js/display.js
@@ -39,8 +39,9 @@ var frameGap;
 var draw = function( drawables) {
   var ballTop = drawables.ball.y - drawables.ball.radius;
   var ballLeft = drawables.ball.x - drawables.ball.radius;
+  console.log(ballTop);
   $("#ball").offset({ top: ballTop, left: ballLeft});
-  console.log(JSON.stringify(drawables.ball));
+  //console.log(JSON.stringify(drawables.ball));
   $("#paddle").offset({top: drawables.paddle.x1, left: drawables.paddle.x2});
 };
 
@@ -51,8 +52,8 @@ window.onload = function() {
   bridge.ready(function() {
     bridge.joinChannel('shot3', { draw: draw}, function(){});
     bridge.getService("physics", function(x) { 
-      x.start([[{x: 0, y: 0}, {x: $(window).width(), y: $(window).height()}]], null, 0, 0);
-      setInterval(function() {x.update(draw)}, 100);
+      x.debug([[{x: 0, y: 0}, {x: 200, y: 200}],[{x: 50, y: 200},{x:250,y:400}]]);
+      setInterval(function() {x.update(draw)}, 200);
     });
   });
 };
