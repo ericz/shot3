88b090df143abe945b0170b6c1f54c0b7b3896b0
Eric Zhang <really.ez@gmail.com>
2012-02-18 03:59:01 -0800

    stuff

--- admin/public/js/display.js
+++ admin/public/js/display.js
@@ -2,8 +2,8 @@ var draw = function( drawables) {
   var ballTop = drawables.ball.y - drawables.ball.radius - translate.y;
   var ballLeft = drawables.ball.x - drawables.ball.radius - translate.x;
   $("#ball").offset({ top: ballTop, left: ballLeft});
-  $("#paddle1").offset({top: drawables.paddle['1'].y1, left: drawables.paddle['1'].x1});
-  $("#paddle2").offset({top: drawables.paddle['2'].y1, left: drawables.paddle['2'].x1});
+  $("#paddle1").offset({top: drawables.paddle['1'].y1 - translate.y, left: drawables.paddle['1'].x1 - translate.x});
+  $("#paddle2").offset({top: drawables.paddle['2'].y1 - translate.y, left: drawables.paddle['2'].x1 - translate.x});
 
 };
 
