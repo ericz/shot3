427727e45ceb09dc61454886d36eeae8b1277f48
Eric Zhang <really.ez@gmail.com>
2012-02-17 22:31:53 -0800

    remove layout

--- admin/server.js
+++ admin/server.js
@@ -9,6 +9,9 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+app.set('view options', {
+  layout: false
+});
 
 
 app.get('/', function(req, res){
