d294672c84c3ba36cde1874ca73e2e9873d68c64
Sharad Vikram <sharad.vikram@gmail.com>
2012-02-18 01:49:49 -0600

    fixed collision

--- physics.js
+++ physics.js
@@ -2,7 +2,7 @@ var Bridge = require('./bridge').Bridge;
 var bridge = new Bridge({host:'136.152.39.187'});
 var ball = {x:120,y:120,radius:20};
 var paddle = {x1:50,y1:50,x2:90,y2:60};
-var vel = {x:5,y:5};
+var vel = {x:10,y:10};
 var running = true;
 var hard, soft;
 physics = {
@@ -19,12 +19,13 @@ physics = {
 		nextXBox = -1;
 		nextYBox = -1;
 		for (var box in boxes){
-			if (util.hardContains(ball,boxes[box])){
+			if (util.softContains(ball,boxes[box])){
 				curBox = box;
 			}
 		}
-		nextXBall = {x:(ball.x+=vel.x),y:(ball.y),radius:ball.radius};
-		nextYBall = {x:(ball.x),y:(ball.y+=vel.y),radius:ball.radius};
+		nextXBall = {x:(ball.x+vel.x),y:(ball.y),radius:ball.radius};
+		nextYBall = {x:(ball.x),y:(ball.y+vel.y),radius:ball.radius};
+		console.log(ball, nextXBall,nextYBall);
 		for (var box in boxes){
 			if (util.softContains(nextXBall,boxes[box])){
 				nextXBox = box;
@@ -32,15 +33,14 @@ physics = {
 			if (util.softContains(nextYBall,boxes[box])){
 				nextYBox = box;
 			}
-		}	
+		}
 		console.log(curBox, nextXBox, nextYBox);
-		if (box != nextXBox){
+		if (curBox != nextXBox){
 			vel.x = -vel.x;
 		}
-		if (box != nextYBox){
+		if (curBox != nextYBox){
 			vel.y = -vel.y;
-		}	
-		console.log(JSON.stringify(ball));
+		}
 		ball.x += vel.x;
 		ball.y += vel.y;	
 		callback({"ball":ball,"paddle":paddle});
@@ -48,10 +48,9 @@ physics = {
 }
 util = {
 	hardContains:function(ball,box){
-		return ((ball.x-ball.radius)>=box[0]] && (ball.x+ball.radius<=box[1]) && ((ball.y+ball.radius)<=box[1].y && (ball.y-ball.radius)>=box[0].y));
+		return ((ball.x-ball.radius)>=box[0] && (ball.x+ball.radius<=box[1]) && ((ball.y+ball.radius)<=box[1].y && (ball.y-ball.radius)>=box[0].y));
 	},
 	softContains:function(ball,box){
-		console.log((ball.y-ball.radius)<=box[1].y);
 		return ((ball.x+ball.radius)>=box[0].x && (ball.x-ball.radius)<=box[1].x) && ((ball.y-ball.radius)<=box[1].y && (ball.y+ball.radius)>=box[0].y)
 	}
 }
