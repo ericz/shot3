d51dae5a6912dde3400b13630b6e63fb7b3cc23d
Michelle Bu <analogmidnight@gmail.com>
2012-02-18 10:58:57 -0800

    snappable stuff

--- admin/public/js/index.js
+++ admin/public/js/index.js
@@ -6,7 +6,7 @@ var physics;
 
 var addRect = function addRect (w, h, callback) {
   var close = $('<div></div>').addClass('close').click(deleteRect);
-  $('<div></div>').data('o', 0).data('cb', callback).dblclick(rotate).addClass('rect').append(close).css({width: w/7, height: h/7, 'background-color': '#'+Math.floor(Math.random()*16777215).toString(16)}).draggable({snap:true, snapMode: 'outer'}).appendTo('#field');
+  $('<div></div>').data('o', 0).data('cb', callback).dblclick(rotate).addClass('rect').append(close).css({width: w/7, height: h/7, 'background-color': '#'+Math.floor(Math.random()*16777215).toString(16)}).draggable({snap:true, snapMode: 'outer', containment: 'parent'}).appendTo('#field');
 };
 
 var send = function send () {
